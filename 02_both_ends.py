ef771f542ff2347ffb7e826434ea6167a8b2cfca
Lucas M. Rodrigues <lucas@scienceweb.com.br>
2020-10-20 16:51:02 -0300

    Resposta exercício 02

--- 02_both_ends.py
+++ 02_both_ends.py
@@ -7,9 +7,13 @@ Exemplo: 'spring' retorna 'spng'. Entretanto, se o tamanho da string
 for menor que 2, retorne uma string vazia.
 """
 
+
 def both_ends(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) >= 2:
+        s = (s[0:2]) + (s[-2:])
+    else:
+        s = ''
+    return s
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
